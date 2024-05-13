e3aa42decbdc6e37a63b83176fe5edef72acff8a
vladvolchek <mskoresh19@gmail.com>
2024-05-13 22:39:36 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // import Home from '../../pages/Home';
-import NavBar from '../Navigation/NavBar';
+import NavBar from '../Navigation/Navigation';
 // import Movie from '../../pages/Movie';
 // import NotFoundPage from '../NotFoundPage';
 // import MovieDetailsPage from '../../pages/MovieDetailsPage';
@@ -9,11 +9,11 @@ import NavBar from '../Navigation/NavBar';
 ///// import Reviews from '../Reviews';
 function App() {
   const Home = lazy(() => import('../../pages/HomePage/Home'));
-  const Movie = lazy(() => import('../../pages/MoviesDetailsPage/Movie'));
+  const Movie = lazy(() => import('../../pages/MoviesPage/Movie'));
   const NotFoundPage = lazy(() => import('../NotFoundPage/NotFoundPage'));
-  const MovieDetailsPage = lazy(() => import('../../pages/MoviesPage/MovieDetailsPage'));
+  const MovieDetailsPage = lazy(() => import('../../pages/MoviesDetailsPage/MovieDetailsPage'));
   const Cast = lazy(() => import('../MovieCast/Cast'));
-  const Reviews = lazy(() => import('../MovieReviews/Reviews'));
+  const Reviews = lazy(() => import('../MovieReviews/MovieReviews'));
 
   return (
     <>
@@ -22,7 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/MoviesDetailsPage" element={<Movie />} />
-          <Route path="/MoviesDetailsPage/:movieId" element={<MovieDetailsPage />}>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
